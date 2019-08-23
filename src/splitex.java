8c2b15e77f952fd125de96f5f89688ca6cb6868c
amarsuchithra <suchi.kasturi@gmail.com>
2019-08-22 20:22:25 -0400

    Basic

---
+++ src/splitex.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class splitex {
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner();
+        System.out.println("Enter a String:");
+        String str=sc.next();
+        String splitStr
+    }
+}
